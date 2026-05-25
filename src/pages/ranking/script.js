12124e4fb0fea896aa9349ad25acc0ff3a10240b
Octávio Augusto <octavio.peregrino80@gmail.com>
2026-05-25 20:44:43 -0300

    feat: implementa painel de ranking, histórico e configurações

---
+++ src/pages/ranking/script.js
@@ -0,0 +1,36 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sFuncao = document.querySelector('#m-funcao')
+const sSalario = document.querySelector('#m-salario')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>${item.nome}</td>   
+  `
+  tbody.appendChild(tr)
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
